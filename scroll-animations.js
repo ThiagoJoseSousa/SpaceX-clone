9416dd934f7e621a5609454dbaece140d462589e
ThiagoJoseSousa <imigrationstudy@gmail.com>
2023-02-06 15:38:33 -0300

    Deleting menu__open div

---
+++ scroll-animations.js
@@ -0,0 +1,49 @@
+const titles__title= document.querySelectorAll(".titles__title")
+const titles__button= document.querySelectorAll(".titles__cta")
+/* name it better:
+intersection observer listens to changes/intersections of a target elemtn either in viewport or ancestor element
+so entries is all intersections of a box ele
+*/
+
+const observer= new IntersectionObserver(
+    entries => {
+        entries.forEach(entry =>{
+            entry.target.classList.toggle("reveal", entry.isIntersecting)
+            if(entry.isIntersecting) {observer.unobserve(entry.target)}
+        })
+    }, {
+        rootMargin:"150px",
+    }
+)
+
+titles__title.forEach(title => {
+    observer.observe(title)
+})
+
+titles__button.forEach(
+    btn=>{
+        observer.observe(btn)
+    }
+)
+
+hideNavbarAsScrolling()
+//showTitlesAsScrolling()
+
+function hideNavbarAsScrolling (){
+    let previousScrollTopDistance=0;
+    const navbar= document.getElementsByClassName('header')[0];
+
+window.addEventListener('scroll', ()=>{
+    // both pageYOffset and documentElement.scrollTop are ways to acess browser scroll
+    let currScrollTopDistance=window.pageYOffset || document.documentElement.scrollTop;
+    if(currScrollTopDistance > previousScrollTopDistance){
+        navbar.classList.add('disappear')
+
+    }
+    if (currScrollTopDistance < previousScrollTopDistance) {
+        navbar.classList.remove('disappear')
+    }
+    previousScrollTopDistance = currScrollTopDistance;
+})
+}
+
